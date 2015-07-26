2c656abd901fe412167de71e748b5b2b27910de3
Chris Bednarski <banzaimonkey@gmail.com>
2015-07-26 16:22:46 -0700

    Initial stub of artifact-override functionality

---
+++ post-processor/artifact-override/artifact.go
@@ -0,0 +1,56 @@
+package arifactoverride
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const BuilderId = "packer.post-processor.artifact-override"
+
+type Artifact struct {
+	files []string
+}
+
+func NewArtifact(files []string) (*Artifact, error) {
+	for _, f := range files {
+		if fi, err := os.Stat(f); err != nil {
+			return nil, err
+		}
+	}
+	artifact := &Artifact{
+		files: files,
+	}
+	return artifact, nil
+}
+
+func (a *Artifact) BuilderId() string {
+	return BuilderId
+}
+
+func (a *Artifact) Files() []string {
+	return a.files
+}
+
+func (a *Artifact) Id() string {
+	return ""
+}
+
+func (a *Artifact) String() string {
+	files := strings.Join(a.files, ", ")
+	return fmt.Sprintf("Created artifact from files: %s", files)
+}
+
+func (a *Artifact) State(name string) interface{} {
+	return nil
+}
+
+func (a *Artifact) Destroy() error {
+	for _, f := range a.files {
+		err := os.Remove(f)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ post-processor/artifact-override/post-processor.go
@@ -0,0 +1,52 @@
+package arifactoverride
+
+import (
+	"github.com/mitchellh/packer/common"
+	"github.com/mitchellh/packer/helper/config"
+	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/template/interpolate"
+)
+
+// The artifact-override post-processor allows you to specify arbitrary files as
+// artifacts. These will override any other artifacts created by the builder.
+// This allows you to use a builder and provisioner to create some file, such as
+// a compiled binary or tarball, extract it from the builder (VM or container)
+// and then save that binary or tarball and throw away the builder.
+
+type Config struct {
+	common.PackerConfig `mapstructure:",squash"`
+
+	Files []string `mapstructure:"files"`
+	Keep  bool     `mapstructure:"keep_input_artifact"`
+
+	ctx interpolate.Context
+}
+
+type PostProcessor struct {
+	config Config
+}
+
+func (p *PostProcessor) Configure(raws ...interface{}) error {
+	err := config.Decode(&p.config, &config.DecodeOpts{
+		Interpolate:        true,
+		InterpolateContext: &p.config.ctx,
+		InterpolateFilter: &interpolate.RenderFilter{
+			Exclude: []string{},
+		},
+	}, raws...)
+	if err != nil {
+		return err
+	}
+
+	if len(p.config.Files) == 0 {
+		return fmt.Error("No files specified in artifact-override; your build will not produce any artifacts")
+	}
+
+	return nil
+}
+
+func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, bool, error) {
+	artifact, err := NewArtifact(p.config.Files)
+
+	return artifact, p.config.Keep, err
+}
